79b0bf7441ff9a276b38e1d775d54f3c713311cc
Carlos Gallo <carlosgallo@poli.ufrj.br>
2021-07-23 12:10:13 -0300

    adds Dashboard styled components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+
 import { View , Text, StyleSheet} from 'react-native'
-import { Container } from './styles';
+
+import { Container, Header, UserInfo, User, Photo, UserGreeting, UserName } from './styles';
 
 export function Dashboard(){
     return(
         <Container>
-            <Text>oi</Text>
+            <Header>
+                  <UserInfo>
+                    <User>
+                        <Photo source ={{uri:'https://www.fillmurray.com/300/300'}} />
+                          <UserGreeting> Olá</UserGreeting>
+                          <UserName>Carlos</UserName>
+                      </User>
+                  </UserInfo>
+                
+                </Header>
         </Container>
     )
 }
